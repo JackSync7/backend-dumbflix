a863116b345799f8b8f12dcd93e57cc956975b26
jeri <jeriutama777abc@gmail.com>
2023-04-28 10:35:29 +0700

    backend mute

---
+++ dto/auth/auth_response.go
@@ -0,0 +1,13 @@
+package authdto
+
+type LoginResponse struct {
+	Email string `gorm:"type: varchar(255)" json:"email"`
+	Token string `gorm:"type: varchar(255)" json:"token"`
+	Role  string `json:"roles" form:"roles" gorm:"type: varchar(255)"`
+	ID    int    `json:"id"`
+}
+
+type RegisterResponse struct {
+	Email string `gorm:"type: varchar(255)" json:"email"`
+	Token string `gorm:"type: varchar(255)" json:"token"`
+}
